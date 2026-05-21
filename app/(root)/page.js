7e70e988a97f8015ea88dc7acb40c861bb1e00ff
Daksh Kapoor <dakshkapoor05@gmail.com>
2026-05-21 23:39:09 +0530

    implement header

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const Home = () => {
   return (
     <>
-      <div className="flex min-h-screen home-wrapper">HOME</div>
+      <div className="flex home-wrapper">HOME</div>
     </>
   );
 };
